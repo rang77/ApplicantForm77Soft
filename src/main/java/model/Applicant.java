71e57ce7d4ba57551150789e9b30328fdac15a15
Ritz Daniel Ang <rang@77global.biz>
2015-10-26 19:15:26 +0800

    Initial Commit

---
+++ src/main/java/model/Applicant.java
@@ -0,0 +1,276 @@
+package model;
+
+import java.util.Date;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Applicant {
+
+	@JsonProperty(value = "Id")
+	private String id;
+
+	@JsonProperty(value = "Name")
+	private String applicantNumber;
+
+	@JsonProperty(value = "AchievementsCertifications__c")
+	private String achievementsCertifications;
+	
+	@JsonProperty(value = "AvailabilityOfEmployment__c")
+	private Date availabilityOfEmployment;
+	
+	@JsonProperty(value = "Birthdate__c")
+	private Date birthdate;
+
+	@JsonProperty(value = "Citizenship__c")
+	private String citizenship;
+
+	@JsonProperty(value = "CivilStatus__c")
+	private String civilStatus;
+
+	@JsonProperty(value = "CurrentPreviousCompany__c")
+	private String currentPreviousCompany;
+
+	@JsonProperty(value = "CurrentPreviousDateEnded__c")
+	private Date currentPreviousDateEnded;
+
+	@JsonProperty(value = "CurrentPreviousDateStarted__c")
+	private Date currentPreviousDateStarted;
+
+	@JsonProperty(value = "EducationAttainments__c")
+	private String educationAttainments;
+
+	@JsonProperty(value = "EmailAddress__c")
+	private String emailAddress;
+
+	@JsonProperty(value = "FirstName__c")
+	private String firstName;
+
+	@JsonProperty(value = "Gender__c")
+	private String gender;
+
+	@JsonProperty(value = "LastName__c")
+	private String lastName;
+
+	@JsonProperty(value = "MiddleName__c")
+	private String middleName;
+
+	@JsonProperty(value = "MobileNumber__c")
+	private String mobileNumber;
+
+	@JsonProperty(value = "CurrentPreviousManager__c")
+	private String currentPreviousManager;
+
+	@JsonProperty(value = "PermanentAddress__c")
+	private String permanentAddress;
+
+	@JsonProperty(value = "CurrentPreviousPosition__c")
+	private String currentPreviousPosition;
+
+	@JsonProperty(value = "PositionApplyingFor__c")
+	private String positionApplyingFor;
+
+	@JsonProperty(value = "PresentAddress__c")
+	private String presentAddress;
+
+	@JsonProperty(value = "Skills__c")
+	private String skills;
+	
+	@JsonProperty(value = "TelephoneNumber__c")
+	private String telephoneNumber;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getApplicantNumber() {
+		return applicantNumber;
+	}
+
+	public void setApplicantNumber(String applicantNumber) {
+		this.applicantNumber = applicantNumber;
+	}
+
+	public String getAchievementsCertifications() {
+		return achievementsCertifications;
+	}
+
+	public void setAchievementsCertifications(String achievementsCertifications) {
+		this.achievementsCertifications = achievementsCertifications;
+	}
+
+	public Date getAvailabilityOfEmployment() {
+		return availabilityOfEmployment;
+	}
+
+	public void setAvailabilityOfEmployment(Date availabilityOfEmployment) {
+		this.availabilityOfEmployment = availabilityOfEmployment;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getCitizenship() {
+		return citizenship;
+	}
+
+	public void setCitizenship(String citizenship) {
+		this.citizenship = citizenship;
+	}
+
+	public String getCivilStatus() {
+		return civilStatus;
+	}
+
+	public void setCivilStatus(String civilStatus) {
+		this.civilStatus = civilStatus;
+	}
+
+	public String getCurrentPreviousCompany() {
+		return currentPreviousCompany;
+	}
+
+	public void setCurrentPreviousCompany(String currentPreviousCompany) {
+		this.currentPreviousCompany = currentPreviousCompany;
+	}
+
+	public Date getCurrentPreviousDateEnded() {
+		return currentPreviousDateEnded;
+	}
+
+	public void setCurrentPreviousDateEnded(Date currentPreviousDateEnded) {
+		this.currentPreviousDateEnded = currentPreviousDateEnded;
+	}
+
+	public Date getCurrentPreviousDateStarted() {
+		return currentPreviousDateStarted;
+	}
+
+	public void setCurrentPreviousDateStarted(Date currentPreviousDateStarted) {
+		this.currentPreviousDateStarted = currentPreviousDateStarted;
+	}
+
+	public String getEducationAttainments() {
+		return educationAttainments;
+	}
+
+	public void setEducationAttainments(String educationAttainments) {
+		this.educationAttainments = educationAttainments;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber__c) {
+		this.mobileNumber = mobileNumber__c;
+	}
+
+	public String getCurrentPreviousManager() {
+		return currentPreviousManager;
+	}
+
+	public void setCurrentPreviousManager(String currentPreviousManager) {
+		this.currentPreviousManager = currentPreviousManager;
+	}
+
+	public String getPermanentAddress() {
+		return permanentAddress;
+	}
+
+	public void setPermanentAddress(String permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+
+	public String getCurrentPreviousPosition() {
+		return currentPreviousPosition;
+	}
+
+	public void setCurrentPreviousPosition(String currentPreviousPosition) {
+		this.currentPreviousPosition = currentPreviousPosition;
+	}
+
+	public String getPositionApplyingFor() {
+		return positionApplyingFor;
+	}
+
+	public void setPositionApplyingFor(String positionApplyingFor) {
+		this.positionApplyingFor = positionApplyingFor;
+	}
+
+	public String getPresentAddress() {
+		return presentAddress;
+	}
+
+	public void setPresentAddress(String presentAddress) {
+		this.presentAddress = presentAddress;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	
+	
+}
---
+++ src/main/java/servlet/InsertApplicantServlet.java
@@ -0,0 +1,132 @@
+package servlet;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.force.api.ApiConfig;
+import com.force.api.ForceApi;
+
+import model.Applicant;
+
+@WebServlet(name = "oauth", urlPatterns = { "/insertApplicant/*", "/insertApplicant" }, initParams = {
+// clientId is 'Consumer Key' in the Remote Access UI
+@WebInitParam(name = "clientId", value = "3MVG9ZL0ppGP5UrC9R5pfGadp9_.sezTYM4KyOofpmNB9S0IumaT57vNAI1j0Xbl6fJInNkjvcIDSCKZ9ypMm"),
+// clientSecret is 'Consumer Secret' in the Remote Access UI
+@WebInitParam(name = "clientSecret", value = "8568904816664843552"),
+// This must be identical to 'Callback URL' in the Remote Access UI
+@WebInitParam(name = "environment", value = "https://login.salesforce.com"), })
+public class InsertApplicantServlet extends HttpServlet {
+	
+	private static final DateFormat DF = new SimpleDateFormat("yyyy-mm-dd");
+	private static final long serialVersionUID = 1L;
+	
+	private static final String USERNAME = "devorg@77soft.com";
+	private static final String PASSWORD = "77GSIDev";
+	private static final String SECURITY_TOKEN = "ta3WbXLee49bAgxiiepNipmA";
+    private String clientId = null;
+    private String clientSecret = null;
+    private String environment = null;
+	 
+    public void init() throws ServletException {
+        clientId = this.getInitParameter("clientId");
+        clientSecret = this.getInitParameter("clientSecret");
+        environment = this.getInitParameter("environment");
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		ApiConfig config = new ApiConfig();
+		config.setUsername(USERNAME);
+		config.setPassword(PASSWORD+SECURITY_TOKEN);
+		config.setClientId(clientId);
+		config.setClientSecret(clientSecret);
+		config.setLoginEndpoint(environment);
+		
+		ForceApi api = new ForceApi(config);
+		
+		Applicant applicant = new Applicant();
+				
+		String achievementsCertifications = req.getParameter("achievementsCertifications");
+		String availabilityOfEmployment = req.getParameter("availabilityOfEmployment");
+		String birthdate = req.getParameter("birthdate");
+		String citizenship = req.getParameter("citizenship");
+		String civilStatus = req.getParameter("civilStatus");
+		String currentPreviousCompany = req.getParameter("currentPreviousCompany");
+		String currentPreviousDateEnded = req.getParameter("currentPreviousDateEnded");
+		String currentPreviousDateStarted = req.getParameter("currentPreviousDateStarted");
+		String educationAttainments = req.getParameter("educationAttainments");
+		String emailAddress = req.getParameter("emailAddress");
+		String firstName = req.getParameter("firstName");
+		String Gender = req.getParameter("Gender");
+		String lastName = req.getParameter("lastName");
+		String middleName = req.getParameter("middleName");
+		String mobileNumber = req.getParameter("mobileNumber");
+		String currentPreviousManager = req.getParameter("currentPreviousManager");
+		String permanentAddress = req.getParameter("permanentAddress");
+		String currentPreviousPosition = req.getParameter("currentPreviousPosition");
+		String positionApplyingFor = req.getParameter("positionApplyingFor");
+		String presentAddress = req.getParameter("presentAddress");
+		String skills = req.getParameter("skills");
+		String telephoneNumber = req.getParameter("telephoneNumber");
+		
+		
+		
+		applicant.setAchievementsCertifications(achievementsCertifications);
+		try {
+			applicant.setAvailabilityOfEmployment(DF.parse(availabilityOfEmployment));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		try {
+			applicant.setBirthdate(DF.parse(birthdate));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		applicant.setCitizenship(citizenship);
+		applicant.setCivilStatus(civilStatus);
+		applicant.setCurrentPreviousCompany(currentPreviousCompany);
+		try {
+			applicant.setCurrentPreviousDateEnded(DF.parse(currentPreviousDateEnded));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		try {
+			applicant.setCurrentPreviousDateStarted(DF.parse(currentPreviousDateStarted));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		applicant.setEducationAttainments(educationAttainments);
+		applicant.setEmailAddress(emailAddress);
+		applicant.setFirstName(firstName);
+		applicant.setGender(Gender);
+		applicant.setLastName(lastName);
+		applicant.setMiddleName(middleName);
+		applicant.setMobileNumber(mobileNumber);
+		applicant.setCurrentPreviousManager(currentPreviousManager);
+		applicant.setPermanentAddress(permanentAddress);
+		applicant.setCurrentPreviousPosition(currentPreviousPosition);
+		applicant.setPositionApplyingFor(positionApplyingFor);
+		applicant.setPresentAddress(presentAddress);
+		applicant.setSkills(skills);
+		applicant.setTelephoneNumber(telephoneNumber);
+		
+		String id = api.createSObject("Applicant__c", applicant);
+		
+		if(id == null || id.length() == 0){
+			response.sendRedirect("/error.html");
+		}else{
+			response.sendRedirect("/success.html");
+		}
+	}
+
+}
