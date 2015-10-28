660eb8ecf1cad2a8b6bcb7394ab81d79a80937e8
rang77 <rang@77global.biz>
2015-10-29 01:32:21 +0800

    Updated max file size constant

--- src/main/java/model/Attachment.java
+++ src/main/java/model/Attachment.java
@@ -6,7 +6,7 @@ import org.codehaus.jackson.annotate.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Attachment {
 	
-	public static final long MAX_FILE_SIZE = Double.valueOf("5E+06").longValue();	
+	public static final long MAX_FILE_SIZE = (1024*1024)*5;	
 	
 	@JsonProperty(value = "Id")
 	private String id;
