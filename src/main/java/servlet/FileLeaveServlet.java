e3b11ea6d23c33b34a7af30cbb02b4cde538ec87
rjosapasap <rjosapasap@gmail.com>
2015-10-28 11:57:25 +0800

    fix fileleaveservlet warning

--- src/main/java/servlet/FileLeaveServlet.java
+++ src/main/java/servlet/FileLeaveServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.TimeZone;
 
 import javax.servlet.ServletException;
