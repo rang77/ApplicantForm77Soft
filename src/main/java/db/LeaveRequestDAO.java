8597030193d64ee9b1827a72713862d4a0a4a7db
Mishael Mae Legaspi <mlegaspi@77global.biz>
2016-06-01 13:05:45 +0800

    HIIII

--- src/main/java/db/LeaveRequestDAO.java
+++ src/main/java/db/LeaveRequestDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import model.LeaveRequest;
 
+//hi
+
 public class LeaveRequestDAO extends SalesforceDAO<LeaveRequest> {
 
 	public static final Class<LeaveRequest> DAOTABLE = LeaveRequest.class;	
